d4ce40d98c5fd5621093bae63fd70d17bffd3655
comet-19 <tomomizu7773@icloud.com>
2023-10-06 14:15:21 +0900

    TODOListの概形を作成

--- src/components/TODOList.tsx
+++ src/components/TODOList.tsx
@@ -9,6 +9,37 @@ function TODOList() {
                 React-v3
             </header>
             <h1>TODOList</h1>
+            <div>
+                <input type="text" /><span><button>追加</button></span>
+            </div>
+            <div className="contents">
+                <table border={1}>
+                    <tr>
+                        <th><input type="checkbox"/></th>
+                        <th>登録日</th>
+                        <th>TODO</th>
+                        <th>削除</th>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox"/></td>
+                        <td>2023/10/03</td>
+                        <td>勉強</td>
+                        <td>削除</td>
+                    </tr>
+                    <tr>
+                    <td><input type="checkbox"/></td>
+                        <td>2023/10/05</td>
+                        <td>ゲーム</td>
+                        <td>削除</td>
+                    </tr>
+                    <tr>
+                    <td><input type="checkbox"/></td>
+                        <td>2023/10/08</td>
+                        <td>掃除</td>
+                        <td>削除</td>
+                    </tr>
+                </table>
+            </div>
         </div>
     );
 }
